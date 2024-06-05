c33765d3045c8b075ccc521059ed471580f052bb
Muhammad Firdaus bin Zubairi <129296479+FirdausZubairi@users.noreply.github.com>
2024-06-06 01:22:56 +0800

    Comment Unused predict Controller

--- src/main/java/com/heroku/java/Controller/PredictController.java
+++ src/main/java/com/heroku/java/Controller/PredictController.java
@@ -1,20 +1,19 @@
 package com.heroku.java.Controller;             
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
 
-import com.heroku.java.Model.Users;
-import com.heroku.java.Services.AccountServices;
+// import com.heroku.java.Model.Users;
+// import com.heroku.java.Services.AccountServices;
 
-import jakarta.servlet.http.HttpSession;
-import java.sql.SQLException;
+// import jakarta.servlet.http.HttpSession;
+// import java.sql.SQLException;
 
 @Controller
 public class PredictController {
-
 
     @GetMapping("/predict-inventory")
     public String predict() {
